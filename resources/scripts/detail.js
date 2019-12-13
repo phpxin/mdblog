c293e4d93812835f2e8436dd13fdbae4abeccce8
phpxin <lixin@bitz.email>
2019-12-13 19:30:51 +0800

    #

--- resources/scripts/detail.js
+++ resources/scripts/detail.js
@@ -5,11 +5,9 @@ hljs.configure({
 document.addEventListener('DOMContentLoaded', (event) => {
     document.querySelectorAll('code').forEach((block) => {
         console.log(block);
+
         hljs.highlightBlock(block);
 
-        // $("code").each(function(){
-        //     console.log(1);
-        //     $(this).html("<ol><li>" + $(this).html().replace(/\n/g,"\n</li><li>") +"\n</li></ol>");
-        // });
+
     });
 });

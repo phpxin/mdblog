4119ae62552dba8e397fd1c96571a848c9d25eeb
phpxin <lixin@bitz.email>
2020-01-02 14:25:44 +0800

    #

--- core/http_context.go
+++ core/http_context.go
@@ -9,7 +9,7 @@ import (
 )
 
 type HttpContext struct {
-	RawReq *http.Request
+	RawReq    *http.Request
 	SessionId string
 }
 
@@ -18,22 +18,22 @@ func (ctx *HttpContext) get(key string) (string, error) {
 
 	rawResult := qStr.Get(key)
 
-	if rawResult=="" {
+	if rawResult == "" {
 		b := ctx.RawReq.Body
 		bodybytes := make([]byte, 1024)
-		rl,err := b.Read(bodybytes)
-		if err!=nil {
-			return "",err
+		rl, err := b.Read(bodybytes)
+		if err != nil {
+			return "", err
 		}
 
-		if rl<=0 {
-			return "",nil
+		if rl <= 0 {
+			return "", nil
 		}
 
 		// todo application/json, x-http-request-urlencoded
-		qStr,err = url.ParseQuery(string(bodybytes))
-		if err!= nil {
-			return "",err
+		qStr, err = url.ParseQuery(string(bodybytes))
+		if err != nil {
+			return "", err
 		}
 		rawResult = qStr.Get(key)
 	}
@@ -42,9 +42,9 @@ func (ctx *HttpContext) get(key string) (string, error) {
 }
 
 func (ctx *HttpContext) GetString(key string, def string) (string, error) {
-	src,err := ctx.get(key)
-	if err!=nil {
-		return def,err
+	src, err := ctx.get(key)
+	if err != nil {
+		return def, err
 	}
 
 	hackStrings := []string{
@@ -53,66 +53,71 @@ func (ctx *HttpContext) GetString(key string, def string) (string, error) {
 		"frame",
 	}
 
-	for _,v := range hackStrings {
-		src = strings.Replace(src, v, "", -1)
+	srcLowercase := strings.ToLower(src)
+
+	for _, v := range hackStrings {
+		// src = strings.Replace(src, v, "", -1)
+		if strings.Index(srcLowercase, v) > -1 {
+			return "", fmt.Errorf("the paramter has invalid words")
+		}
 	}
 
-	return src,nil
+	return src, nil
 }
 
 func (ctx *HttpContext) GetInt32(key string, def int32) (int32, error) {
 
-	src,err := ctx.get(key)
-	if err!=nil {
-		return def,err
+	src, err := ctx.get(key)
+	if err != nil {
+		return def, err
 	}
 
-	if src=="" {
-		return def,fmt.Errorf("not found")
+	if src == "" {
+		return def, fmt.Errorf("not found")
 	}
 
-	r,err := strconv.ParseInt(src, 10, 32)
-	if err!=nil {
-		return def,err
+	r, err := strconv.ParseInt(src, 10, 32)
+	if err != nil {
+		return def, err
 	}
 
-	return int32(r),nil
+	return int32(r), nil
 }
 
 func (ctx *HttpContext) GetInt64(key string, def int64) (int64, error) {
 
-	src,err := ctx.get(key)
-	if err!=nil {
-		return def,err
+	src, err := ctx.get(key)
+	if err != nil {
+		return def, err
 	}
 
-	if src=="" {
-		return def,fmt.Errorf("not found")
+	if src == "" {
+		return def, fmt.Errorf("not found")
 	}
 
-	r,err := strconv.ParseInt(src, 10, 64)
-	if err!=nil {
-		return def,err
+	r, err := strconv.ParseInt(src, 10, 64)
+	if err != nil {
+		return def, err
 	}
 
-	return r,nil
+	return r, nil
 }
 
 func (ctx *HttpContext) GetFloat64(key string, def float64) (float64, error) {
 
-	src,err := ctx.get(key)
-	if err!=nil {
-		return def,err
+	src, err := ctx.get(key)
+	if err != nil {
+		return def, err
 	}
 
-	if src=="" {
-		return def,fmt.Errorf("not found")
+	if src == "" {
+		return def, fmt.Errorf("not found")
 	}
 
-	r,err := strconv.ParseFloat(src, 64)
-	if err!=nil {
-		return def,err
+	r, err := strconv.ParseFloat(src, 64)
+	if err != nil {
+		return def, err
 	}
 
-	return r,nil
+	return r, nil
 }
